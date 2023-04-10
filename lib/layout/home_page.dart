a511ae699d3f69b9dfc1404784c2745bdc862ed1
Magaretha14 <magaretha.wahyunda@gmail.com>
2023-04-10 16:53:25 +0700

    add untuk display data

--- lib/layout/home_page.dart
+++ lib/layout/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import '../model/contact.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,9 +11,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Contact> ct = [];
+  late Box<Contact> box;
+
+  @override
+  void initState() {
+    super.initState();
+    box = Hive.box('dbContact');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: ct.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                leading: const Icon(Icons.person),
+                title: Text(ct[index].name),
+                subtitle: Column(
+                  children: [Text(ct[index].email), Text(ct[index].phone)],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Icon(Icons.add),
